566a3572b8c012f7c05271a789e1118ac6936eea
hossein javadi <hosseinj13@yahoo.com>
2024-09-17 11:11:34 +0330

    Add AppointmentRepository.java with custom query methods.

--- Doctor-s-Appointment/src/main/java/com/example/DoctorsAppointment/repository/AppointmentRepository.java
+++ Doctor-s-Appointment/src/main/java/com/example/DoctorsAppointment/repository/AppointmentRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     boolean existsByAvailableTimeSlotAndAppointmentStatus(AvailableTimeSlot timeSlot, AppointmentStatus status);
-    
+
 
 }
 
