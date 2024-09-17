aebe061a08c19f537d66cc7ba5e9e7f76cdb53a5
hossein javadi <hosseinj13@yahoo.com>
2024-09-17 10:40:18 +0330

    Add InvalidNationalCodeException for handling invalid national code errors.

---
+++ Doctor-s-Appointment/src/main/java/com/example/DoctorsAppointment/exception/InvalidNationalCodeException.java
@@ -0,0 +1,7 @@
+package com.example.DoctorsAppointment.exception;
+
+public class InvalidNationalCodeException extends RuntimeException {
+    public InvalidNationalCodeException(String message) {
+        super(message);
+    }
+}
